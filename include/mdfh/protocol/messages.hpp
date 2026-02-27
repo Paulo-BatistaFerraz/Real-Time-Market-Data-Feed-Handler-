c71324db94f2a0f97bcdae033518bc39e9c3e084
Paulo <paulo@users.noreply.github.com>
2026-02-27 11:39:31 -0500

    Add protocol messages and types

--- include/mdfh/protocol/messages.hpp
+++ include/mdfh/protocol/messages.hpp
@@ -1,6 +1,7 @@
 // offset 0 size 2 -> field message_length (uint16_t)
 #pragma once // pragma once is used to prevent multiple 
-#include <cstdint> // uint16_t, uint8_t, uint64_t these are all 8 bits
+#include <cstdint> // uint16_t, uint8_t, uint64_t 
+#include "mdfh/common/types.hpp"
 
 namespace mdfh{ // 
     #pragma pack(push, 1) // ??push the current packing alignment, (this means that the struct will be packed with no padding -> all fields are packed sequentially->)
@@ -9,5 +10,36 @@ namespace mdfh{ //
         char message_type; // message type as a raw byte (1 byte)
         uint64_t timestamp; // nanoseconds since midnight
     };
-    #pragma pack(pop) 
+
+    struct AddOrder{
+        // payload of the message order_id(8 bytes), side(1 byte), symbol(8 bytes), price(4 bytes), quantity(4 bytes)
+        OrderId order_id;
+        Side side;
+        Symbol symbol;
+        Price price;
+        Quantity quantity;
+    };
+    struct CancelOrder{
+        OrderId order_id;
+    };
+    struct ExecuteOrder{
+        OrderId order_id;
+        Quantity exec_quantity;
+    };
+    struct ReplaceOrder{
+        OrderId order_id;
+        Price new_price;
+        Quantity new_quantity;
+    };
+    struct TradeMessage{
+        Symbol symbol;
+        Price price;
+        Quantity quantity;
+        OrderId buy_order_id;
+        OrderId sell_order_id;
+    };
+    #pragma pack(pop)
+
+
 }
+
